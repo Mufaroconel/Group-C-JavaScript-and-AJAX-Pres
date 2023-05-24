42cc3cada3613a6b2f83946cc11457d68facc579
Mufaroconel <128815143+Mufaroconel@users.noreply.github.com>
2023-05-24 11:10:53 +0200

    Create style.css

---
+++ style.css
@@ -0,0 +1,232 @@
+html{
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: 500;
+    font-size: 20px;
+    box-sizing: border-box;
+    color: white;
+}
+
+*,*::before,*::after{
+    box-sizing: inherit;
+}
+
+
+img.logo{
+        display: block;
+        margin: auto;
+        margin-top: 10px;
+        border-radius: 10px;
+}
+
+#header nav{
+    width: 100vw;
+    height: 150px;
+    background-color: rgb(21, 22, 23);
+    color: whitesmoke;
+    position: fixed;
+    top: 0;
+    margin-left: -8px;
+}
+#header nav h4{
+}
+
+
+.container{
+    padding-top: 150px;
+    margin: 20px;
+    text-align: center;
+    box-shadow: 2px 3px 7px 0px rgba(0,0,0,0.35);
+    overflow: hidden;
+    background-color: #272a2f
+}
+
+.container p{
+    background-color: #272a2f;
+    border-radius: 5px;
+    color: white;
+    text-align: left;
+}
+
+.container h1, h2{
+    color: #2ab2af;
+}
+
+#variables p{
+    text-align: left;
+}
+
+#for-loop{
+    text-align: left;
+    margin-left: -40px;
+}
+
+#numbers, #colors{
+    background-color: #272a2f;
+    margin-left: -50px;
+    margin-top: 30px;
+}
+body{
+    background-color: #272a2f;
+}
+#bold{
+    font-weight: 800;
+    font-size: larger;
+    color: red;
+}
+
+#variables-paragraph, #bold{
+    display: inline-block;
+}
+
+#list{
+    margin-top: 20px;
+    margin-bottom: 20px;
+    text-align: left;
+}
+
+#while-loop, #for-each-loop{
+    text-align: left;
+}
+#numbers, #colors, #states{
+    list-style: none;
+  }
+
+#functionparagraph{
+    text-align: left;
+}
+
+div{
+    margin-top: 50px;
+}
+a{
+    text-align: center;
+    font-size: 0.7rem;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+
+#object p{
+    margin-left: 30px;
+}
+#colors{
+    color: white;
+}
+
+li{
+    text-align: left;
+}
+
+#loops{
+    background-color: white;
+}
+
+#javascript-form-processing{
+    text-align: left;
+}
+
+#pop-up-boxes{
+    text-align: left;
+}
+button{
+    color: white;
+    background-color: #c0df34;
+    border: none;
+    padding: 10px;
+    width: 100%;
+    border-radius: 5px;
+}
+
+button:hover{
+    cursor: pointer;
+    background-color: #A9C42D;
+    transform: scale(1.1);
+}
+
+#mufaro, #admire, #marlon, #chawaz, #macdonald, #swish, #crispen, #mj, #gmt, #tana, #ngoni{
+    display: inline-block;
+    margin: 10px;
+}
+
+#sarah, #previey, #noku, #anesu{
+    display: inline-block;
+    margin: 20px;
+}
+
+#presentersfemaleimg{
+    margin: 20px;
+}
+
+img.presentersimg{
+    display: block;
+    margin: auto;
+    margin-top: 10px;
+    border-radius: 50%;
+}
+
+img.presentersfemaleimg{
+   
+    margin: auto;
+    margin-top: 10px;
+    border-radius: 50%;
+}
+
+#italic{
+    font-style: italic;
+    font-size: 0.8rem;
+    
+}
+
+.container #italic p{
+    color: lime;
+}
+
+#presenters{
+    background-color: rgb(21, 22, 23);
+}
+
+#presenters h1{
+    text-align: center;
+}
+
+#mabhebhi, #mafia {
+    text-align: center;
+    margin: 10px;
+}
+#mabhebhi{
+    list-style: none;
+}
+#mafia li{
+    list-style: none;
+}
+
+#atags a{
+      font-style: italic;
+   color: #A9C42D;
+   font-size: 17.3px;
+}
+
+#pxmargin{
+   margin: 100px;
+}
+
+
+
+#ajaxxmlhttprequestimage, #ajaxxmlhttpreques{
+    display: inline-block;
+}
+
+@media(min-width: 576px){
+    .container p{
+        
+    }
+    #header h1{
+        font-size: 25px;
+    }   
+}
+
+
+
+
+
+
